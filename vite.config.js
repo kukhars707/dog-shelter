ca22cb99aff2c4a03c98da8c90384415dc54be9b
sergejkuhar <kukhar.s707@gmail.com>
2022-08-05 13:57:44 +0300

    fixed base path

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "dog-shelter.github.io/",
   plugins: [vue()],
   resolve: {
     alias: {
